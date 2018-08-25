8816e3dd311aae4c14f5fe2a9bf7deddaf344825
Tinh Truong <tinh.truong@papercut.com>
2018-08-25 22:21:16 +1000

    Display result in table format

--- src/bitbucket.org/tinhtruong/aur-ood/main.go
+++ src/bitbucket.org/tinhtruong/aur-ood/main.go
@@ -9,10 +9,15 @@ import (
 
 func main() {
 	ctx := context.Background()
-	update, err := aur.GetPackageUpdates(ctx)
+	updates, err := aur.GetPackageUpdates(ctx)
 	if err != nil {
 		panic(err)
 	}
-
-	fmt.Printf("update: %+v", update)
+	fmt.Printf("┌───────────────────────────────┬─────────────────────┬──────────────────┐\n")
+	fmt.Printf("│  Package Name                 │  Installed Version  │  Latest Version  │\n")
+	fmt.Printf("├───────────────────────────────┼─────────────────────┼──────────────────┤\n")
+	for _, update := range updates {
+		fmt.Printf("│  %-29s│  %-19s│  %-16s│\n", update.Name, update.InstalledVersion, update.LatestVersion)
+	}
+	fmt.Printf("└───────────────────────────────┴─────────────────────┴──────────────────┘\n")
 }
